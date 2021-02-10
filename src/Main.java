396a5517f8f1d761fc855116fbe7efde3a750819
aleksanderkuzmenkov <alexkuzdeveloping@gmail.com>
2021-02-10 13:03:30 +0100

    Die Daten werden in eine Datei reingeschrieben.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,16 @@ public class Main {
 
 		Model model = new Model();
 
-		model.addNewCurrentAccount("b350", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewCurrentAccount("c350", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewCurrentAccount("c351", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewCurrentAccount("c352", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewCurrentAccount("c353", "Max Mustermann", 550, 20, new GregorianCalendar());
+
+		model.addNewSavingAccount("s350", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewSavingAccount("s351", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewSavingAccount("s352", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewSavingAccount("s353", "Max Mustermann", 550, 20, new GregorianCalendar());
+		model.addNewSavingAccount("s354", "Max Mustermann", 550, 20, new GregorianCalendar());
 
 		Controller controller = new Controller("Bank Program");
 
